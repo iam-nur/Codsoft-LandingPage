7cc548798443a5b136c098cebfe2cc9531e614e5
Nuru Husen <133539190+iam-nur@users.noreply.github.com>
2024-08-14 14:12:33 +0300

    update
    updating essencial source codes

---
+++ src/js/script.js
@@ -0,0 +1,68 @@
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".nav_bar");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav_open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+ 
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav_open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".nav_bar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+
+
